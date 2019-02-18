09f4cfaef234cc3a77393a609a63f3d7222ed528
JeanOlivier <Jean.Olivier.Simoneau@USherbrooke.ca>
2019-02-18 15:32:03 -0500

    acorrs_otf.py: Corrected init for non-FFT case & cleanup

--- acorrs_otf.py
+++ acorrs_otf.py
@@ -63,6 +63,7 @@ class ACorrUpTo():
             else:
                 fft = False
 
+
         fftflag = "FFT" if fft else ""
         if fft and k>fftchunk:
             fftchunk = int(2**ceil(log2(k))) # Ceil to power of two
@@ -71,6 +72,7 @@ class ACorrUpTo():
         self.k = k
         self.res = zeros(self.k)
         self.len = fftchunk
+        self.fft = fft
         
         assert self.dtype in self.atype.keys(), TypeError("Unsupported type ({})".format(self.dtype))
         
@@ -79,8 +81,10 @@ class ACorrUpTo():
         self.accum = lib["ACorrUpTo{fft}_{dtype}_accumulate".format(dtype=self.dtype, fft=fftflag)]
         self.aCorrs = lib["ACorrUpTo{fft}_{dtype}_get_aCorrs".format(dtype=self.dtype, fft=fftflag)]
         self.dest = lib["ACorrUpTo{fft}_{dtype}_destroy".format(dtype=self.dtype, fft=fftflag)]
-        self._init.argtypes = (c_int, c_int)
-        #self._init.argtypes = (c_int,)
+        if fft:
+            self._init.argtypes = (c_int, c_int)
+        else:
+            self._init.argtypes = (c_int,)
         self.aCorrs.argtypes = (ndpointer(c_double, shape=(k,)), c_int)
 
         self.accumulate_rk = lib["ACorrUpTo{fft}_{dtype}_accumulate_rk".format(dtype=self.dtype, fft=fftflag)]
@@ -90,14 +94,6 @@ class ACorrUpTo():
             ndpointer(c_double, shape=(k,)), 
             c_int
         )
-       
-        #self.accumulate_rk_FFTW = lib["ACorrUpTo_{dtype}_accumulate_rk_FFTW".format(dtype=self.dtype)]
-        #self.accumulate_rk_FFTW.argtypes = (
-        #    ndpointer(dtype=self.atype[self.dtype], shape=(len(x),)),
-        #    c_uint64,
-        #    ndpointer(c_double, shape=(k,)), 
-        #    c_int
-        #)
 
         # Actually initializing the C++ class
         self.init()
@@ -108,8 +104,10 @@ class ACorrUpTo():
             
 
     def init(self):
-        self._init(self.k, self.len)
-        #self._init(self.k)
+        if self.fft:
+            self._init(self.k, self.len)
+        else:
+            self._init(self.k)
 
     def accumulate(self, x):
         # Accumulating
@@ -145,99 +143,4 @@ class ACorrUpTo():
 
     def __del__(self):
         self.destroy()
-
-
-# Python functions for testing:
-def rk_py(x,k):
-    n = len(x)
-    c = zeros(k, double)
-    for i in range(n-k):
-            for j in range(k):
-                    c[j] += double(x[i])*x[i+j]
-    return double(c)
-
-def rk(x,k):
-    res = zeros(k)
-    with ACorrUpTo(k,x.dtype.name) as aa:
-        aa.accumulate_rk(x, len(x)-k, res, k)
-    return res
-
-#def rk_FFTW(x,k):
-#    res = zeros(k)
-#    with ACorrUpTo(k,x.dtype.name) as aa:
-#        aa.accumulate_rk_FFTW(x, len(x)-k, res, k)
-#    return res
-
-
-
-def rkFFT(x,lags):
-        '''fft, pad 0s, non partial'''
-        n=len(x)
-        # pad 0s to 2n-1
-        ext_size=2*n-1
-        # nearest power of 2
-        fsize=2**numpy.ceil(numpy.log2(ext_size)).astype('int')
-        # do fft and ifft
-        cf=numpy.fft.rfft(x,fsize)
-        sf=cf.conjugate()*cf
-        corr=numpy.fft.irfft(sf)
-        corr=corr[:lags]
-        return corr
-
-def rkFFT_long(x,k,l=8192,parallel=8):
-    #TODO: Implement in C++ with FFTW
-    # Python parallel version only faster if k is long.
-    # It should be N*log(l) complexity vs N*k for direct rk.
-    if k>l/2:
-        l=2*k+1
-    n = len(x)
-    num = n//l
-    kfactors = array([double(n)/(n-j*num) for j in range(k)])
-    y = x[:x.size-x.size%l]
-    y.resize(num,l)
-    if parallel:
-        from pathos.multiprocessing import Pool
-        p = Pool(parallel)
-        res = sum(p.map(lambda i:rkFFT(i,k),y), axis=0)
-        del p
-    else:
-        res = zeros(k)
-        for i in y:
-            res += rkFFT(i,k)
-    return res*kfactors
-
-
-def aCorr(x,k):
-    n = len(x)
-    c = zeros(k)
-    for i in range(n-k):
-            for j in range(k):
-                    c[j] += ((x[i]-x.mean())*(x[i+j]-x.mean()))/(len(x)-j)
-    for i in range(n-k, n):
-            for j in range(n-i):
-                    c[j] += ((x[i]-x.mean())*(x[i+j]-x.mean()))/(len(x)-j)
-    return c
-
-def aCorrFFT(x,k):
-    b = rfft(x)
-    b[0]=0 # This removes the mean
-    c = b*b.conj()
-    d = irfft(c)
-    return array([i*x.size/(x.size-j) for j,i in enumerate(d)]) # Correct-k normalization
-
-def autocorr3(x,lags):
-        '''fft, pad 0s, non partial'''
-        n=len(x)
-        # pad 0s to 2n-1
-        ext_size=2*n-1
-        # nearest power of 2
-        fsize=2**numpy.ceil(numpy.log2(ext_size)).astype('int')
-        xp=x-numpy.mean(x)
-        var=numpy.var(x)
-        # do fft and ifft
-        cf=numpy.fft.rfft(xp,fsize)
-        sf=cf.conjugate()*cf
-        corr=numpy.fft.irfft(sf)
-        corr=corr[:lags]
-        return array([i/(n-j) for j,i in enumerate(corr)])
 
